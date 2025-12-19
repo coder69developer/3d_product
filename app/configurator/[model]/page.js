b8c2aca5824f39d1f29ef2ed5a5a131a4cfd87d4
coder69developer <113542720+coder69developer@users.noreply.github.com>
2025-12-19 12:38:50 +0100

    make changes to create github page

--- app/configurator/[model]/page.js
+++ app/configurator/[model]/page.js
@@ -1,5 +1,14 @@
 import ConfiguratorLayout from "@/configurator/ConfiguratorLayout";
 
+export async function generateStaticParams() {
+  return [
+    { model: 'bottle' },
+    { model: 'spray_bottle' },
+    { model: 'container' },
+    { model: 'kanster' },
+  ]
+}
+
 export default async function ConfiguratorPage({ params }) {
   const { model } = await params;
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/3d_product/' : '',
 };
 
 export default nextConfig;
